df63dea80673e54edd55fe3a05ee2fb8afd9f46d
Edward Turner <eddturner@ebi.ac.uk>
2014-04-11 11:49:47 +0100

    promises promises.

--- libs/album-timeline.js
+++ libs/album-timeline.js
@@ -1,113 +1,153 @@
-   var width = 1000,
-       height = 400,
-       color = d3.scale.category20b();
+var width = 1000,
+    height = 400,
+    color = d3.scale.category20b();
 
-   var PADDING = 50;
-   var MAX_RADIUS;
+var PADDING = 50;
+var MAX_RADIUS;
 
-   var svg = d3.select("#container")
-       .append("svg")
-       .attr("width", width)
-       .attr("height", height);
+var svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-   var lineheight = 20;
+var lineheight = 20;
 
-   // handy: http://alignedleft.com/content/03-tutorials/01-d3/160-axes/5.html
-   // http://alignedleft.com/tutorials/d3/axes
-   var dataset = [];
+// handy: http://alignedleft.com/content/03-tutorials/01-d3/160-axes/5.html
+// http://alignedleft.com/tutorials/d3/axes
+var dataset = [];
+var albums = [];
 
-   function getData(dataset) {
-       $.ajax({
-           url: 'http://ws.spotify.com/lookup/1/.json',
-           type: 'GET',
-           dataType: 'json',
-           data: {
-               uri: 'spotify:artist:6g0mn3tzAds6aVeUYRsryU',
-               extras: 'albumdetail'
-           }
-       })
-           .done(function(data) {
-               $.each(data.artist.albums, function(index, item) {
-                   getAlbumScore(item.album.href);
-	           dataset.push(item.album.released);
-console.log("pushing - "+ item.album.released);
-               });
+function getData() {
+    return $.ajax({
+        url: 'http://ws.spotify.com/lookup/1/.json',
+        type: 'GET',
+        dataType: 'json',
+        data: {
+            uri: 'spotify:artist:6g0mn3tzAds6aVeUYRsryU',
+            extras: 'albumdetail'
+        }
+    })
+        .fail(function () {
+            console.log("error");
+        })
+}
+
+function getAlbumScore(albumHref) {
+    return $.ajax({
+        url: 'http://ws.spotify.com/lookup/1/.json',
+        type: 'GET',
+        dataType: 'json',
+        data: {
+            uri: albumHref,
+            extras: 'trackdetail'
+        }
+    }).done(function (data) {
+        var total_pop = 0;
+        // Get popularity for each track and calculate average
+        $.each(data.album.tracks, function (index, track) {
+            total_pop += track.popularity * 100;
+        });
+        console.log("href: " + albumHref);
+
+        console.log(total_pop);
+        dataset.push([data.album.released, total_pop]);
+        console.log("pushing - " + data.album.released + ", " + total_pop);
+
+    }).fail(function () {
+        console.log("Error retrieving info for album " + albumId);
+    });
+}
+
+$.when(
+    getData())
+    .then(
+    // after getData()
+    function (data) {
+        var node = svg.selectAll('.album-node')
+            .data(data.artist.albums)
+            .enter()
+            .append('g')
+            .attr('class', 'album-node');
+
+        node.append('text')
+            .attr('y', function (d, i) {
+                return lineheight * i;
+            })
+            .text(function (d) {
+                console.log(d);
+                return d.album.name;
+            });
 
 
-               var node = svg.selectAll('.album-node')
-                   .data(data.artist.albums)
-                   .enter()
-                   .append('g')
-                   .attr('class', 'album-node');
+        var calls = [];
+        $.each(data.artist.albums, function (index, item) {
+            calls.push(getAlbumScore(item.album.href));
 
-               node.append('text')
-                   .attr('y', function(d, i) {
-                       return lineheight * i;
-                   })
-                   .text(function(d) {
-                       console.log(d);
-                       return d.album.name;
-                   });
-
-   	       var xScale = d3.scale.linear()
-		 .domain([d3.min(dataset, function(d) { return d; }), d3.max(dataset, function(d) { return d; })])
-		 .range([PADDING, width - PADDING * 2]);
-	       svg.selectAll("circle")
-			   .data(dataset)
-			   .enter()
-			   .append("circle")
-			   .attr("cx", function(d) {
-			   		return xScale(d);
-			   })
-			   .attr("cy", function(d) {
-					return 100;
-//			   		return yScale(d[1]);
-			   })
-			   .attr("r", function(d) {
-					return 10;
-					// will be based on score
-			   		//return rScale(d[1]);
-			   });
+        });
+        $.when.apply($, calls)
+            .then(function (data) {
+                console.log("------fetched album score!");
+                console.log(data);
+                setupAxes(data[0]);
+            })
 
 
-   		// create axes
-		console.log(dataset);
-		var xAxis = d3.svg.axis()
-			  .scale(xScale)
-			  .orient("bottom")
-			  .ticks(5)
-			  .tickFormat(d3.format("<"));
-		svg.append("g")
-			.attr("class", "axis")
-			.attr("transform", "translate(0," + (height - PADDING) + ")")
-			.call(xAxis);
+    });
 
-           })
-           .fail(function() {
-               console.log("error");
-           })
-   }
 
-   function getAlbumScore(albumHref) {
-       $.ajax({
-           url: 'http://ws.spotify.com/lookup/1/.json',
-           type: 'GET',
-           dataType: 'json',
-           data: {
-               uri: albumHref,
-               extras: 'trackdetail'
-           }
-       }).done(function(data) {
-           var total_pop = 0;
-           // Get popularity for each track and calculate average
-           $.each(data.album.tracks, function(index, track) {
-               total_pop += track.popularity * 100;
-           });
-           console.log(total_pop);
+function setupAxes(data) {
+    var xScale = d3.scale.linear()
+        .domain([d3.min(dataset, function (d) {
+            return d[0];
+        }), d3.max(dataset, function (d) {
+            return d[0];
+        })])
+        .range([PADDING, width - PADDING * 2]);
 
-       }).fail(function() {
-           console.log("Error retrieving info for album " + albumId);
-       });
-   }
+    var yScale = d3.scale.linear()
+        .domain([d3.min(dataset, function (d) {
+            return d[1];
+        }), d3.max(dataset, function (d) {
+            return d[1];
+        })])
+        .range([height - PADDING, PADDING]);
 
-   getData(dataset);
+    var rScale = d3.scale.linear()
+        .domain([d3.min(dataset, function (d) {
+            return d[1];
+        }), d3.max(dataset, function (d) {
+            return d[1];
+        })])
+        .range([0, 50]);
+
+    svg.selectAll("circle")
+        .data(dataset)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) {
+            console.log(">>> " + d);
+            return xScale(d[0]);
+        })
+        .attr("cy", function (d) {
+            // i recks would be nice if this was based on say, public popularity?... dunno, just thinking, say, downloads vs critical review might be interesting.
+            // ... but for now, based on rScale
+            return yScale(d[1]);
+        })
+        .attr("r", function (d) {
+            // will be based on score
+            return rScale(d[1]);
+        });
+
+
+    // create axes
+    console.log(dataset);
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient("bottom")
+        .ticks(5)
+        .tickFormat(d3.format("<"));
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0," + (height - PADDING) + ")")
+        .call(xAxis);
+}
